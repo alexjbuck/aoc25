17bb61571b9dd1e6392bd4e5695dcb82c9b13b01
alexjbuck <alexjbuck@gmail.com>
2025-12-05 01:38:42 -0500

    Add script for making new days from template

---
+++ template/src/main.rs
@@ -0,0 +1,17 @@
+/// # Panics
+/// This function panics if the input cannot be read correctly
+/// # Errors
+/// This function returns an `AoCError` with variants "Cache" and "Network"
+pub fn main() {
+    let year = 2025;
+    let day = _;
+    libaoc::evaluate(part_1, part_2, year, day);
+}
+
+fn part_1(_input: &str) -> usize {
+    todo!()
+}
+
+fn part_2(_input: &str) -> usize {
+    todo!()
+}
