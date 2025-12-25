1e8bad983181b644f9f06fc9bf4e5158eb947f06
alexjbuck <alexjbuck@gmail.com>
2025-12-25 10:31:32 -0500

    Day 3 part 2

--- days/day03/src/main.rs
+++ days/day03/src/main.rs
@@ -8,13 +8,6 @@ pub fn main() {
     libaoc::evaluate(part_1, part_2, year, day);
 }
 
-#[inline]
-fn first_max(s: &str) -> (usize, char) {
-    s.chars()
-        .enumerate()
-        .fold((0, '0'), |acc, (i, c)| if c > acc.1 { (i, c) } else { acc })
-}
-
 fn max_joltage(line: &str) -> usize {
     let len = line.len();
     let (i, tens) = first_max(&line[..len - 1]);
@@ -23,12 +16,43 @@ fn max_joltage(line: &str) -> usize {
     val as usize
 }
 
+#[inline]
+fn first_max(s: &str) -> (usize, char) {
+    s.chars()
+        .enumerate()
+        .fold((0, '0'), |acc, (i, c)| if c > acc.1 { (i, c) } else { acc })
+}
+fn recursive_joltage(line: &str, mut remaining: usize, mut value: usize) -> (&str, usize, usize) {
+    // Base case
+    if remaining == 0 {
+        return (line, remaining, value);
+    }
+    let len = line.len();
+    // len = 6 (0..6), rem=4, len-rem=2  (0, 1, 2, 3, 4, 5), 4, 0
+    // [0..=2] = (0,1,2) len 3, max=2, i=2, left (3, 4, 5) (new rem = 3)
+    // len = 3 (1..4), rem=3, len-rem=0 (1,2,3), 3, 0
+    // [0..=0] = (1,) len 1
+    let (i, next_digit) = first_max(&line[..=len - remaining]);
+    remaining -= 1;
+    value = 10 * value
+        + usize::try_from(
+            next_digit
+                .to_digit(10)
+                .expect("The input strings should all be valid base-10 (radix-10) digits"),
+        )
+        .expect("A single digit should be a valid usize");
+    recursive_joltage(&line[i + 1..], remaining, value)
+}
+
 fn part_1(input: &str) -> usize {
     input.lines().map(max_joltage).sum()
 }
 
-fn part_2(_input: &str) -> usize {
-    todo!()
+fn part_2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| recursive_joltage(line, 12, 0).2)
+        .sum()
 }
 
 #[cfg(test)]
